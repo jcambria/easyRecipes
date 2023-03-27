a8b108883995bdc7523dfe559f89669ad41c46c6
MarQuan McCarrel <marquanmccarrel@gmail.com>
2023-03-27 15:47:48 -0400

    Application setup

---
+++ src/main/java/mealsbydad/MealsbydadApplication.java
@@ -0,0 +1,13 @@
+package mealsbydad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealsbydadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealsbydadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mealsbydad/MealsbydadApplicationTests.java
@@ -0,0 +1,13 @@
+package mealsbydad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsbydadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
