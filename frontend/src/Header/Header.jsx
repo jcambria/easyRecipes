71ce9e99e1a7d961522cc4127898382f3d9a0c14
Justin  Cambria <justincambria@Justins-MacBook-Air.local>
2023-03-29 15:10:00 -0400

    vs transfer

---
+++ frontend/src/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { GiBoneKnife, GiForkKnifeSpoon, GiSpoon } from 'react-icons/gi';
+import './header.css';
+
+
+
+
+const Header = () => {
+  return (
+    <div className='app__header app__wrapper section__padding' id='home'>
+        <div className='app__wrapper_info'>
+          <img src="" alt="" /> 
+         <h1 className='app__header-h1'>  <GiSpoon className='spoon' /> To Help Dads Everywhere</h1>
+
+         <p id='slogon'>Meals created by dads for dads.</p>
+
+         <p>Meals for Dad is a company that was created to help dads cook and make amazing meals for their families. The company provides tools, resources, and support for dads who want to improve their cooking skills and provide delicious meals for their loved ones. Meals for Dad aims to empower dads to take an active role in meal preparation and provide healthy, nutritious, and flavorful meals for their families. With a focus on simplifying meal preparation and making cooking enjoyable, Meals for Dad offers a variety of recipes, cooking tips, and tools to help dads create memorable meals.
+
+</p>
+         <button id='explorebutton'>Explore More</button>
+       
+         <img id='steak' src="https://loveincorporated.blob.core.windows.net/contentimages/gallery/42a33494-9333-4b6c-aeb6-1687e5a63506-resting-steak.jpg" alt="" />
+        
+        </div>
+        <div className='app__wrapper_img'>
+        </div>
+    </div>
+  )
+}
+
+export default Header;
