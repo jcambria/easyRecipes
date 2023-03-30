0f37ff2bb1287f89d7e4cd10a0145f2d5a3a499c
Ayub <egalayub2@gmail.com>
2023-03-30 14:24:14 -0400

    Created Resource Component and added tailwindcss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
 import RecipeSearchBar from './components/FindRecipe/RecipeSearchBar';
 import RecipeCard from './components/RecipeCard/RecipeCard';
+import Card from './components/Resources/Card';
+import Slider1, {Timer,Measurment} from './components/Resources/Slider1';
 
 
 function App() {
@@ -13,8 +15,8 @@ function App() {
       <Header />
       <RecipeSearchBar />
       <RecipeCard  />
-
-
+      <Card />
+  
       {/* <Recipe /> */}
     </div>
   );
