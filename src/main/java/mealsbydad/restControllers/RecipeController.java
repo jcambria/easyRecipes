2bcaa900b20987ad19846deeb166a14a11baac78
Jimmy McCue <xstreamfishn@gmail.com>
2023-03-31 11:00:40 -0400

    test failing

--- src/main/java/mealsbydad/restControllers/RecipeController.java
+++ src/main/java/mealsbydad/restControllers/RecipeController.java
@@ -2,22 +2,27 @@ package mealsbydad.restControllers;
 
 import mealsbydad.entities.Recipe;
 import mealsbydad.respositories.RecipeRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import mealsbydad.respositories.UserRepository;
+import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin
 public class RecipeController {
 
     final private RecipeRepository recipeRepository;
+    final private UserRepository userRepository;
 
-    public RecipeController(RecipeRepository recipeRepository) {
+    public RecipeController(RecipeRepository recipeRepository, UserRepository userRepository) {
         this.recipeRepository = recipeRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/api/recipes")
     public Iterable<Recipe> getRecipes() {
         return recipeRepository.findAll();
+    }
+
+    @PostMapping("/api/recipe")
+    public Recipe postRecipe(final @RequestBody Recipe recipe) {
+        return recipeRepository.save(recipe);
     }
 }
