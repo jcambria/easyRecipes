676bd485ae740c833b193ba77d2d1546345f72bd
Justin  Cambria <justincambria@Justins-MacBook-Air.local>
2023-04-06 13:29:53 -0400

    changed some labels in navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -8,25 +8,20 @@ import SignInButton from "./SignInButton";
 import Register from "./Register";
 import RegisterButton from "./RegisterButton";
 import LogoutButton from "./LogoutButton";
-
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   const [isShowLogin, setIsShowLogin] = useState(true);
   const [isShowRegister, setIsShowRegister] = useState(true);
   const [user, setUser] = useState();
-
   const handleLoginClick = () => {
     setIsShowLogin((isShowLogin) => !isShowLogin);
   };
-
   const handleLogoutClick = () => {
     setUser();
   };
-
   const handleRegisterClick = () => {
     setIsShowRegister((isShowRegister) => !isShowRegister);
   };
-
   return (
     <div>
       <nav className="app_navbar">
@@ -35,13 +30,6 @@ const Navbar = () => {
           {/* <img src="https://images.cooltext.com/5651464.png" alt="" /> */}
           <img src="https://images.cooltext.com/5650975.png" alt="logo" />
         </div>
-
-        <ul className='app__navbar-links'>
-            <li className='p__opensans'><a href="#home">Home</a></li>
-            <li className='p__opensans'><a href="#recipes">Find Recipes</a></li>
-            <li className='p__opensans'><a href="#resources">Resources</a></li>
-            <li className='p__opensans'><a href="#contact">Add Recipes</a></li>
-
         <ul className="app__navbar-links">
           <li className="p__opensans">
             <a href="#home">Home</a>
@@ -53,15 +41,12 @@ const Navbar = () => {
             <a href="#resources">Resources</a>
           </li>
           <li className="p__opensans">
-            <a href="#contact">Contact</a>
+            <a href="#recipes">Add Recipes</a>
           </li>
-
         </ul>
         <div className="app__navbar-login">
           <div className="navbar"></div>
-
           <div />
-
           {user && (
             <>
               <LogoutButton handleLogoutClick={handleLogoutClick} user={user} />
@@ -70,7 +55,6 @@ const Navbar = () => {
               </a>
             </>
           )}
-
           {!user && (
             <>
               <SignInButton handleLoginClick={handleLoginClick} />
@@ -84,18 +68,6 @@ const Navbar = () => {
             fontSize={27}
             onClick={() => setToggleMenu(true)}
           />
-
-
-            {toggleMenu && (
-          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
-            <MdOutlineRestaurantMenu className='overlay__close'  fontSize={27} onClick={() => setToggleMenu(false)} />
-            <ul className='app__nav-smallscreen_links'>
-                <li className='p__opensans'><a onClick={() => setToggleMenu(false)} href="#home">Home</a></li>
-                <li className='p__opensans'><a onClick={() => setToggleMenu(false)} href="#recipes">Find Recipe</a></li>
-                <li className='p__opensans'><a onClick={() => setToggleMenu(false)} href="#resources">Resources</a></li>
-                <li className='p__opensans'><a onClick={() => setToggleMenu(false)} href="#contact">Add Recipes</a></li>
-        </ul>   
-
           {toggleMenu && (
             <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
               <MdOutlineRestaurantMenu
@@ -120,12 +92,11 @@ const Navbar = () => {
                   </a>
                 </li>
                 <li className="p__opensans">
-                  <a onClick={() => setToggleMenu(false)} href="#contact">
-                    Contact
+                  <a onClick={() => setToggleMenu(false)} href="#recipes">
+                    Add Recipes
                   </a>
                 </li>
               </ul>
-
             </div>
           )}
         </div>
@@ -145,5 +116,4 @@ const Navbar = () => {
     </div>
   );
 };
-
 export default Navbar;
