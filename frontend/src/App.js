d26cad5853d5ed70ecc1076371fce60e576c71e5
Jimmy McCue <38022840+jimmymccue@users.noreply.github.com>
2023-04-10 18:52:43 -0400

    Update App.js
    removed (>>>>>>> origin/main)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
       <ImageGallery images= {['https://a.cdn-hotels.com/gdcs/production0/d1513/35c1c89e-408c-4449-9abe-f109068f40c0.jpg?impolicy=fcrop&w=800&h=533&q=medium',
 
       {/* <ImageGallery images= {['https://a.cdn-hotels.com/gdcs/production0/d1513/35c1c89e-408c-4449-9abe-f109068f40c0.jpg?impolicy=fcrop&w=800&h=533&q=medium',
->>>>>>> origin/main
+
       'https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000',
       'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?cs=srgb&dl=pexels-ash-376464.jpg&fm=jpg',
       'https://itsallgoodvegan.com/wp-content/uploads/2020/03/img_3427.jpg' ]} /> */}
