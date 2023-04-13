c483600adaf1c290ba1d600667c1424615afdee3
Jimmy McCue <38022840+jimmymccue@users.noreply.github.com>
2023-04-12 23:07:29 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
 import RecipeSearchBar from './components/FindRecipe/RecipeSearchBar';
+import RecipeCard from './components/RecipeCard/RecipeCard';
 import Slider1 from './components/Resources/Slider1';
 import Footer from './components/Footer/Footer';
 import ShoppingList from './ShoppingList/ShoppingList';
